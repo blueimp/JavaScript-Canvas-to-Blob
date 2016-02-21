9aa4b5f65669779453ca3e7b53eddaa35a16db21
Sebastian Tschan <mail@blueimp.net>
2016-02-21 18:18:48 +0100

    Use JavaScript Standard Style.

--- test/test.js
+++ test/test.js
@@ -11,85 +11,82 @@
 
 /*global window, describe, it, Blob */
 
-(function (expect) {
-    'use strict';
+;(function (expect) {
+  'use strict'
 
-    // 80x60px GIF image (color black, base64 data):
-    var b64Data = 'R0lGODdhUAA8AIABAAAAAP///ywAAAAAUAA8AAACS4SPqcvtD6' +
-            'OctNqLs968+w+G4kiW5omm6sq27gvH8kzX9o3n+s73/g8MCofE' +
-            'ovGITCqXzKbzCY1Kp9Sq9YrNarfcrvcLDovH5PKsAAA7',
-        imageUrl = 'data:image/gif;base64,' + b64Data,
-        blob = window.dataURLtoBlob && window.dataURLtoBlob(imageUrl);
+  // 80x60px GIF image (color black, base64 data):
+  var b64Data = 'R0lGODdhUAA8AIABAAAAAP///ywAAAAAUAA8AAACS4SPqcvtD6' +
+      'OctNqLs968+w+G4kiW5omm6sq27gvH8kzX9o3n+s73/g8MCofE' +
+      'ovGITCqXzKbzCY1Kp9Sq9YrNarfcrvcLDovH5PKsAAA7'
+  var imageUrl = 'data:image/gif;base64,' + b64Data
+  var blob = window.dataURLtoBlob && window.dataURLtoBlob(imageUrl)
 
-    describe('canvas.toBlob', function () {
+  describe('canvas.toBlob', function () {
+    it('Converts a canvas element to a blob and passes it to the callback function', function (done) {
+      window.loadImage(blob, function (canvas) {
+        canvas.toBlob(
+          function (newBlob) {
+            done()
+            expect(newBlob).to.be.a(Blob)
+          }
+        )
+      }, {canvas: true})
+    })
 
-        it('Converts a canvas element to a blob and passes it to the callback function', function (done) {
-            window.loadImage(blob, function (canvas) {
-                canvas.toBlob(
-                    function (newBlob) {
-                        done();
-                        expect(newBlob).to.be.a(Blob);
-                    }
-                );
-            }, {canvas: true});
-        });
+    it('Converts a canvas element to a PNG blob', function (done) {
+      window.loadImage(blob, function (canvas) {
+        canvas.toBlob(
+          function (newBlob) {
+            done()
+            expect(newBlob.type).to.be('image/png')
+          },
+          'image/png'
+        )
+      }, {canvas: true})
+    })
 
-        it('Converts a canvas element to a PNG blob', function (done) {
-            window.loadImage(blob, function (canvas) {
-                canvas.toBlob(
-                    function (newBlob) {
-                        done();
-                        expect(newBlob.type).to.be('image/png');
-                    },
-                    'image/png'
-                );
-            }, {canvas: true});
-        });
+    it('Converts a canvas element to a JPG blob', function (done) {
+      window.loadImage(blob, function (canvas) {
+        canvas.toBlob(
+          function (newBlob) {
+            done()
+            expect(newBlob.type).to.be('image/jpeg')
+          },
+          'image/jpeg'
+        )
+      }, {canvas: true})
+    })
 
-        it('Converts a canvas element to a JPG blob', function (done) {
-            window.loadImage(blob, function (canvas) {
-                canvas.toBlob(
-                    function (newBlob) {
-                        done();
-                        expect(newBlob.type).to.be('image/jpeg');
-                    },
-                    'image/jpeg'
-                );
-            }, {canvas: true});
-        });
+    it('Keeps the aspect ratio of the canvas image', function (done) {
+      window.loadImage(blob, function (canvas) {
+        canvas.toBlob(
+          function (newBlob) {
+            window.loadImage(newBlob, function (img) {
+              done()
+              expect(img.width).to.be(canvas.width)
+              expect(img.height).to.be(canvas.height)
+            })
+          }
+        )
+      }, {canvas: true})
+    })
 
-        it('Keeps the aspect ratio of the canvas image', function (done) {
-            window.loadImage(blob, function (canvas) {
-                canvas.toBlob(
-                    function (newBlob) {
-                        window.loadImage(newBlob, function (img) {
-                            done();
-                            expect(img.width).to.be(canvas.width);
-                            expect(img.height).to.be(canvas.height);
-                        });
-                    }
-                );
-            }, {canvas: true});
-        });
-
-        it('Keeps the image data of the canvas image', function (done) {
-            window.loadImage(blob, function (canvas) {
-                canvas.toBlob(
-                    function (newBlob) {
-                        window.loadImage(newBlob, function (newCanvas) {
-                            var canvasData = canvas.getContext('2d')
-                                    .getImageData(0, 0, canvas.width, canvas.height),
-                                newCanvasData = newCanvas.getContext('2d')
-                                    .getImageData(0, 0, newCanvas.width, newCanvas.height);
-                            done();
-                            expect(canvasData.width).to.be(newCanvasData.width);
-                            expect(canvasData.height).to.be(newCanvasData.height);
-                        }, {canvas: true});
-                    }
-                );
-            }, {canvas: true});
-        });
-
-    });
-
-}(this.expect));
+    it('Keeps the image data of the canvas image', function (done) {
+      window.loadImage(blob, function (canvas) {
+        canvas.toBlob(
+          function (newBlob) {
+            window.loadImage(newBlob, function (newCanvas) {
+              var canvasData = canvas.getContext('2d')
+                  .getImageData(0, 0, canvas.width, canvas.height)
+              var newCanvasData = newCanvas.getContext('2d')
+                  .getImageData(0, 0, newCanvas.width, newCanvas.height)
+              done()
+              expect(canvasData.width).to.be(newCanvasData.width)
+              expect(canvasData.height).to.be(newCanvasData.height)
+            }, {canvas: true})
+          }
+        )
+      }, {canvas: true})
+    })
+  })
+}(this.expect))
